05e34ffd6363a9f2c9077f4d898ce1bd0d0a3cdb
Reed1279 <30813808+Reed1279@users.noreply.github.com>
2017-08-16 16:57:13 -0400

    Add files via upload

---
+++ Project/AreaandCircumferenceofaCircle/Area and Circumference of a Circle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_and_Circumference_of_a_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Program to find the area and circumference of a circle
+            double radius;
+            double PI = 3.142;
+            double area, circle, diameter;
+            Console.Write("Enter the Radius of a Circle ");
+            radius = double.Parse(Console.ReadLine());
+            area = PI * (radius * radius);
+            circle = 2 * PI * radius;
+            diameter = 2 * PI;
+            Console.WriteLine("The area of a Circle is " + area);
+            Console.WriteLine("The Circumference of a Circle is " + circle);
+            Console.WriteLine("The Diameter of a Circle is " + diameter);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Project/QuadraticEquation/Quadratic Equation.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub buCompute_Click(sender As Object, e As EventArgs) Handles buCompute.Click
+        Dim a As Single
+        Dim b As Single
+        Dim c As Single
+        Dim x1 As Single
+        Dim x2 As Single
+        a = Val(txtA.Text)
+        b = Val(txtB.Text)
+        c = Val(txtC.Text)
+        x1 = (-b + Math.Sqrt(Math.Abs(b ^ 2 - 4 * a * c))) / (2 * a)
+        x2 = (-b - Math.Sqrt(Math.Abs(b ^ 2 - 4 * a * c))) / (2 * a)
+        lblr.Text = "The value of x is " & x1 & " or " & x2
+
+
+    End Sub
+End Class
