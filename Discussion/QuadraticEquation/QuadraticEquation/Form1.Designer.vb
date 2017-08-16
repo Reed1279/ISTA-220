2c6aaf261a0b1ff09db807f58c661c6206a4af12
Reed1279 <30813808+Reed1279@users.noreply.github.com>
2017-08-16 13:28:02 -0400

    Add files via upload

---
+++ Discussion/QuadraticEquation/QuadraticEquation/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblr = New System.Windows.Forms.Label()
+        Me.buCompute = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Side A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Side C"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 81)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Side B"
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(73, 27)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(100, 20)
+        Me.txtA.TabIndex = 3
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(73, 122)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(100, 20)
+        Me.txtC.TabIndex = 4
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(73, 74)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(100, 20)
+        Me.txtB.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 161)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Result"
+        '
+        'lblr
+        '
+        Me.lblr.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblr.Location = New System.Drawing.Point(31, 174)
+        Me.lblr.Name = "lblr"
+        Me.lblr.Size = New System.Drawing.Size(226, 34)
+        Me.lblr.TabIndex = 7
+        '
+        'buCompute
+        '
+        Me.buCompute.Location = New System.Drawing.Point(188, 71)
+        Me.buCompute.Name = "buCompute"
+        Me.buCompute.Size = New System.Drawing.Size(69, 23)
+        Me.buCompute.TabIndex = 8
+        Me.buCompute.Text = "Compute"
+        Me.buCompute.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.buCompute)
+        Me.Controls.Add(Me.lblr)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtB)
+        Me.Controls.Add(Me.txtC)
+        Me.Controls.Add(Me.txtA)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Quadratic Equation"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtA As TextBox
+    Friend WithEvents txtC As TextBox
+    Friend WithEvents txtB As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblr As Label
+    Friend WithEvents buCompute As Button
+End Class
