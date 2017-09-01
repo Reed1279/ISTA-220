b56434c7639ac8a1f74cf46d7f1dd4d7460eb879
Tyler Reed <30813808+Reed1279@users.noreply.github.com>
2017-09-01 16:48:28 -0400

    Add files via upload

---
+++ Exercise/WeaponSquad/WeaponSquad/Gun1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponSquad
+{
+    class Gun1 : Squad
+    {
+
+
+        public void G1Fire()
+        {
+            Console.WriteLine("Gun 1 responds with Gun 1 Rapid Rate on me");
+        }
+
+        public void G1ShiftFire()
+        {
+            Console.WriteLine("Gun 1 replys Shift Fire on 3, Shift Fire Confirmed");
+        }
+
+        public override void Kill()
+        {
+            Console.WriteLine("Executes guidance given from his SL with precise execution");
+        }
+    }
+}
