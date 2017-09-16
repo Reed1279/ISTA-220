ac3262bdafc221338fbe268fce2a3a3177d55b24
Tyler Reed <30813808+Reed1279@users.noreply.github.com>
2017-09-16 16:21:56 -0400

    Add files via upload

---
+++ Exercise/WeaponSquad/SquadLeader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponSquad
+{
+    class SquadLeader : Squad
+    {
+
+        public virtual void CMDFire()
+        {
+            Console.WriteLine("SL yell's out Rapid Rate of Fire on 1");
+        }
+
+        public virtual void CMDShiftFire()
+        {
+            Console.WriteLine("SL directs guns to Shift Fire on 3");
+        }
+
+        public override void Kill()
+        {
+            Console.WriteLine("Gives commands to his Squad for execution");
+        }
+    }
+}
