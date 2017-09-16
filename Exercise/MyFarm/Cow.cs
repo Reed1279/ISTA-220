79042617285797737345d3d0c05bb1068aeb7971
Tyler Reed <30813808+Reed1279@users.noreply.github.com>
2017-09-16 16:20:43 -0400

    Add files via upload

---
+++ Exercise/MyFarm/Cow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFarm
+{
+    class Cow
+
+    {
+        public void name()
+        {
+            Console.WriteLine("My name is Milk Dud.");
+        }
+
+        public void eat()
+        {
+            Console.WriteLine("I like to eat grass and stuff.");
+        }
+
+        public void sound()
+        {
+            Console.WriteLine("I can only speak in Mooo.");
+        }
+
+
+        public void like()
+        {
+            Console.WriteLine("I like standing around.  Unless it is going to storm, then I lay down.");
+        }
+    }
+}
---
+++ Exercise/MyFarm/Pig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFarm
+{
+    class Pig
+    {
+        public void name()
+        {
+            Console.WriteLine("My name is Fat Amy.");
+        }
+
+        public void eat()
+        {
+            Console.WriteLine("I like to eat stuff that is put into the pan for me.");
+        }
+
+        public void sound()
+        {
+            Console.WriteLine("I say oink and sometimes I grunt when I want food.");
+        }
+
+
+        public void like()
+        {
+            Console.WriteLine("I love to roll around in the mud.  It is so good for your skin.");
+        }
+    }
+}
