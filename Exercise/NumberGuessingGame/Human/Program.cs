e335bd1d7f422dd17dd4b8297d43b00693511b67
Tyler Reed <30813808+Reed1279@users.noreply.github.com>
2017-09-22 11:32:43 -0400

    Add files via upload

---
+++ Exercise/NumberGuessingGame/Human/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+		{
+
+			Random r = new Random();
+
+			int val = r.Next(1, 100);
+			int guess = 0;
+			bool correct = false;
+
+			Console.WriteLine("I'm thinking of a number between 1 and 100.  Can you guess what it is?");
+
+			while (!correct)
+			{
+				Console.Write("Guess: ");
+				string input = Console.ReadLine();
+
+				if (!int.TryParse(input, out guess))
+				{
+					Console.WriteLine("HA HA HA, That's not a number.");
+					continue;
+				}
+
+				if (guess < val)
+				{
+					Console.WriteLine("Nope, the number I'm thinking is higher.  Guess again.");
+				}
+				else if (guess > val)
+				{
+					Console.WriteLine("No, the number I'm thinking is lower.  Guess again.");
+				}
+				else
+				{
+					correct = true;
+					Console.WriteLine("You guessed it!  Good thing to, I was getting worried.");
+				}
+			}
+		}
+	}
+}
